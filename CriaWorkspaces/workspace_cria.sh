e7a80cf001913f502c3e73c4f71f98282981c565
Leonardo Fernandes <38868428+odranoelsf@users.noreply.github.com>
2020-12-06 20:37:59 -0300

    update version 1.4

--- CriaWorkspaces/workspace_cria.sh
+++ CriaWorkspaces/workspace_cria.sh
@@ -1,39 +1,43 @@
 #Created By: Leonardo Fernandes
 #Email: odranoelsf@gmail.com
-#Version: v1.3
+#Version: v1.4
 #!/bin/bash
-NUM=0
+NUM=1
 
-echo "Digite o DirectoryId"
+echo "Type the DirectoryId"
 read directory;
-echo "Digite o BundleID"
+echo "Type the BundleID"
 read bundleid;
 
-TotalUsers=$(cat ~/usuarios.txt | wc -l)
+echo "This script creates one user per time, please check the log.txt file to check if any error occurred"
+
+TotalUsers=$(cat usuarios.txt | wc -l)
 
 while [ $NUM -le $TotalUsers ]; do
-userws=$(awk NR==$NUM+1 ~/usuarios.txt)
+userws=$(awk NR==$NUM usuarios.txt)
 
 echo -e \
-'{ \n 
-  \n 
-"Workspaces" : [ \n 
-\n 
-    { \n 
-        "DirectoryId" : ''"'${directory}'"'', \n 
-	      "UserName" : ''"'${userws}'"'', \n 
-        "BundleId" : ''"'${bundleid}''"'', \n 
-        "WorkspaceProperties": { \n 
-          "RunningMode": "ALWAYS_ON", \n 
-          "RootVolumeSizeGib": 80, \n 
-          "UserVolumeSizeGib": 10 \n 
-        } \n 
-    } \n 
-] \n 
+'{ \n
+  \n
+"Workspaces" : [ \n
+\n
+    { \n
+        "DirectoryId" : ''"'${directory}'"'', \n
+              "UserName" : ''"'${userws}'"'', \n
+        "BundleId" : ''"'${bundleid}'"'', \n
+        "WorkspaceProperties": { \n
+          "RunningMode": "ALWAYS_ON", \n
+          "RootVolumeSizeGib": 80, \n
+          "UserVolumeSizeGib": 10 \n
+        } \n
+    } \n
+] \n
 }' > workspace.json
 
 
-aws workspaces create-workspaces --cli-input-json file://"~/workspace.json" >> log.txt
+aws workspaces create-workspaces --cli-input-json file://"workspace.json" >> log.txt
+
+echo "$NUMº user created"
 
 let NUM=NUM+1;
 done
