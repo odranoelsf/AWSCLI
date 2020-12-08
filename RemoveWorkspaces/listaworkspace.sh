20d6d0f933686e2b617b5d037ac63a2b5da0ac93
Leonardo Fernandes <38868428+odranoelsf@users.noreply.github.com>
2020-12-08 19:30:41 -0300

    update version 1.1

--- RemoveWorkspaces/listaworkspace.sh
+++ RemoveWorkspaces/listaworkspace.sh
@@ -1,6 +1,6 @@
 #Created By: Leonardo Fernandes
 #Email: odranoelsf@gmail.com
-#Version: v1.0
+#Version: v1.1
 #!/bin/bash
 NUM=1
 TotalOperador=$(cat operadores.txt | wc -l)
@@ -12,20 +12,23 @@ while [ $NUM -le $TotalOperador ]; do
 operadorws=$(awk NR==$NUM operadores.txt)
 
 aws workspaces describe-workspaces --directory-id $DirectoryId --user-name "$operadorws" | grep WorkspaceId >> computers.txt
-#aws workspaces describe-workspaces --directory-id d-90677273bf --user-name "$operadorws" | grep WorkspaceId >> computers2.txt
-#aws workspaces describe-workspaces --directory-id d-9067733938 --user-name "$operadorws" | grep WorkspaceId >> computers3.txt
 
 let NUM=NUM+1;
 done
 
+sed -i 's/\            "WorkspaceId": //g' computers.txt
+sed -i 's/\ //g' computers.txt
+sed -i 's/\,//g' computers.txt
+sed -i 's/\"//g' computers.txt
+
 NUM1=1
-TotalComputer=$(cat computer.txt | wc -l)
+TotalComputer=$(cat computers.txt | wc -l)
 #
 while [ $NUM1 -le $TotalComputer ]; do
 computerws=$(awk NR==$NUM1 computers.txt)
 
 aws workspaces terminate-workspaces --terminate-workspace-requests $computerws >> terminate.log
-
+echo "$operadorws removed"
 let NUM1=NUM1+1;
 
 done
